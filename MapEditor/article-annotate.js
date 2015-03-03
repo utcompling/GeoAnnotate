a71a6a7991d170c34a3d32903393e21d5acdd7d2
Grant DeLozier <grantdelozier@gmail.com>
2015-03-03 00:58:47 -0600

    fixed click listener on spans

--- MapEditor/article-annotate.js
+++ MapEditor/article-annotate.js
@@ -48,28 +48,12 @@ function removeArticle(){
 
 }
 
-function checkRange(element){
-    var textarea1 = document.getElementById("col2text");
-    //var click_position = textarea1.selectionStart;
-    var highlight_range = rangy.getSelection().getRangeAt(0).toCharacterRange(textarea1)
-    var highlight_start = highlight_range.start;
-    var highlight_end = highlight_range.end;
-    //window.alert(highlight_start, highlight_end);
-    for(var a = 0; a < article_ranges.length; a++ ){
-        //window.alert("Article Range: " + article_ranges[a][0] + " " + article_ranges[a][1]);
-        if (highlight_start >= article_ranges[a][0] && highlight_start <= article_ranges[a][1]){
-            window.alert("Clicked inside article");
-            var range = rangy.createRange();
-            range.selectNodeContents(element);
-            //var node = element;
-            //debugger;
-            //range.selectNode($(this));
-            //range.setStartAndEnd(textarea1, article_ranges[a][0], article_ranges[a][1]);
-            var sel = rangy.getSelection();
-            sel.setSingleRange(range);
-
-        }
-    }
+function spanClick(element){
+    window.alert("Clicked inside article");
+    var range = rangy.createRange();
+    range.selectNodeContents(element);
+    var sel = rangy.getSelection();
+    sel.setSingleRange(range);
 }
 
 function replaceRange(s, start, end, substitute) {
@@ -93,7 +77,7 @@ function init() {
 
     highlighter.addClassApplier(rangy.createClassApplier("highlight", {
         ignoreWhiteSpace: true,
-        elementProperties: {onclick:"checkRange(this)"},
+        elementAttributes: {onclick:"spanClick(this)"},
         tagNames: ["span", "a"]
     }));
 

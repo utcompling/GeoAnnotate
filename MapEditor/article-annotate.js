2eb113019a9c4c868603fec66e917ef999278ef6
Grant DeLozier <grantdelozier@gmail.com>
2015-03-02 23:55:26 -0600

    article-annotate app: Attempting to force selection on article range click

--- MapEditor/article-annotate.js
+++ MapEditor/article-annotate.js
@@ -1,3 +1,5 @@
+var article_ranges = [];
+
 
 function match_ranges(sometext, re) {
     var matches = [];
@@ -33,6 +35,13 @@ function removeHighlightFromSelectedText() {
 
 function addArticle(){
     highlightSelectedText();
+    var range = rangy.createRange();
+    var highlight_range = rangy.getSelection().getRangeAt(0).toCharacterRange(document.getElementById('col2text'))
+    var highlight_start = highlight_range.start;
+    var highlight_end = highlight_range.end;
+    article_ranges.push(([highlight_start, highlight_end]));
+    //var highlight_range = range.toCharacterRange("col2text")
+    window.alert(highlight_start + " " + highlight_end);
 }
 
 function removeArticle(){
@@ -40,8 +49,24 @@ function removeArticle(){
 }
 
 function checkRange(){
-    var textarea1 = document.getElementById("col2text")
-    var click_position = textarea1.selectionStart;
+    var textarea1 = document.getElementById("col2text");
+    //var click_position = textarea1.selectionStart;
+    var highlight_range = rangy.getSelection().getRangeAt(0).toCharacterRange(textarea1)
+    var highlight_start = highlight_range.start;
+    var highlight_end = highlight_range.end;
+    //window.alert(highlight_start, highlight_end);
+    for(var a = 0; a < article_ranges.length; a++ ){
+        //window.alert("Article Range: " + article_ranges[a][0] + " " + article_ranges[a][1]);
+        if (highlight_start >= article_ranges[a][0] && highlight_start <= article_ranges[a][1]){
+            window.alert("Clicked inside article")
+            var range = rangy.createRange(textarea1);
+            //range.selectNodeContents(textarea1);
+            range.setStartAndEnd(textarea1, article_ranges[a][0], textarea1, article_ranges[a][1])
+            var sel = rangy.getSelection()
+            sel.setSingleRange(range)
+
+        }
+    }
 }
 
 function replaceRange(s, start, end, substitute) {
